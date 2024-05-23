a5e90fbcde3ef8bbfb913679141ec1e712aaffb7
gu9gu9gu <hujin2020@hotmail.com>
2024-05-23 18:42:30 +0800

    Update

--- main.go
+++ main.go
@@ -9,34 +9,45 @@ import (
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Printf("Usage: %s <domain> <interval-seconds>\n", os.Args[0])
+	if len(os.Args) < 3 {
+		fmt.Printf("Usage: %s <domain> <interval-seconds> <record-type>\n", os.Args[0])
+		fmt.Println("Record type can be 'A' or 'AAAA'")
 		os.Exit(1)
 	}
 
 	domain := os.Args[1]
+	intervalStr := os.Args[2]
+	recordType := os.Args[3]
 
-	interval := 1.0
-	if len(os.Args) == 3 {
-		f, err := strconv.ParseFloat(os.Args[2], 64)
-		if err != nil {
-			fmt.Printf("failed to parse interval(%s): %v\n", os.Args[2], err)
-			os.Exit(1)
-		}
-		interval = f
+	interval, err := strconv.ParseFloat(intervalStr, 64)
+	if err != nil {
+		fmt.Printf("failed to parse interval(%s): %v\n", intervalStr, err)
+		os.Exit(1)
 	}
 
 	duration := time.Millisecond * time.Duration(interval*1000)
 
 	for {
-		resolveDomain(domain)
+		resolveDomain(domain, recordType)
 		time.Sleep(duration)
 	}
 }
 
-func resolveDomain(domain string) {
+func resolveDomain(domain, recordType string) {
 	startTime := time.Now()
-	_, err := net.LookupIP(domain)
+	var ips []net.IP
+	var err error
+
+	switch recordType {
+	case "A":
+		ips, err = net.LookupIP(domain)
+	case "AAAA":
+		ips, err = net.LookupAAAA(domain)
+	default:
+		fmt.Printf("Invalid record type: %s. Use 'A' or 'AAAA'.\n", recordType)
+		return
+	}
+
 	elapsed := time.Since(startTime)
 
 	if err != nil {
@@ -45,4 +56,7 @@ func resolveDomain(domain string) {
 	}
 
 	fmt.Printf("%v : %.3f ms\n", startTime.Format(time.StampMilli), elapsed.Seconds()*1000)
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
 }
